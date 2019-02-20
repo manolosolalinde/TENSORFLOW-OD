285766210983c21a6f5b7313e19230fae8fe87d0
Manuel Solalinde <manolosolalinde@gmail.com>
2019-02-20 20:14:38 -0300

    changed csv to cvs

--- training_demo/scripts/preprocessing/xml_to_cvs.sh
+++ training_demo/scripts/preprocessing/xml_to_cvs.sh
@@ -2,7 +2,7 @@ IMAGES="detect_n_label/example_data/datalab_dog_dataset/images"
 ANNOTATIONS="detect_n_label/example_data/datalab_dog_dataset/annotations"
 
 # Create train data:
-python3 detect_n_label/xml_to_csv.py -i $IMAGES/train -o $ANNOTATIONS/train_labels.csv
+python3 detect_n_label/xml_to_cvs.py -i $IMAGES/train -o $ANNOTATIONS/train_labels.csv
 
 # Create test data:
-python3 detect_n_label/xml_to_csv.py -i $IMAGES/test -o $ANNOTATIONS/test_labels.csv
+python3 detect_n_label/xml_to_cvs.py -i $IMAGES/test -o $ANNOTATIONS/test_labels.csv
