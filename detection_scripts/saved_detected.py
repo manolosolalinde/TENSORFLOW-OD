55c8f3410edd063b78cf1d2ac5011826cff23027
Manuel Solalinde <manolosolalinde@gmail.com>
2019-02-26 16:37:20 -0300

    included import tarfile in function

--- detection_scripts/saved_detected.py
+++ detection_scripts/saved_detected.py
@@ -29,13 +29,14 @@ def load_image_into_numpy_array(image):
 # MODEL_FILE = 'ssd_mobilenet_v1_coco_11_06_2017.tar.gz'
 # DOWNLOAD_BASE = 'http://download.tensorflow.org/models/object_detection/'
 def download_model(DOWNLOAD_BASE,MODEL_FILE):
-  opener = urllib.request.URLopener()
-  opener.retrieve(DOWNLOAD_BASE + MODEL_FILE, MODEL_FILE)
-  tar_file = tarfile.open(MODEL_FILE)
-  for file in tar_file.getmembers():
-    file_name = os.path.basename(file.name)
-    if 'frozen_inference_graph.pb' in file_name:
-      tar_file.extract(file, os.getcwd())
+    import tarfile
+    opener = urllib.request.URLopener()
+    opener.retrieve(DOWNLOAD_BASE + MODEL_FILE, MODEL_FILE)
+    tar_file = tarfile.open(MODEL_FILE)
+    for file in tar_file.getmembers():
+        file_name = os.path.basename(file.name)
+        if 'frozen_inference_graph.pb' in file_name:
+            tar_file.extract(file, os.getcwd())
 
 
 def save_xml(prefix, pd_output, category_index,outputshape,cropedshape):
